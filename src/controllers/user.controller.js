ff9e53033c65c1fb87f9b4fa0ff6a86ec069ace1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-08-31 13:38:45 -0600

    controller login user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -40,3 +40,25 @@ exports.createUser = tryCatch( async (req, res, next) => {
     data: userNew
   })
 })
+
+.exports.loginUser = tryCatch( async (req, res, next) => {
+  const { email, password } = req.body
+  const userFind = User.findOne({email})
+
+  if (!userFind) {
+    return next( new ErrorApp('Login fail', 404))
+  }
+
+  const isPassword = bcrypt.compareSync(userFind.password, password)
+
+  if (!isPassword) {
+    return next( new ErrorApp('Login fail', 404))
+  }
+
+  return res.status(202).json({
+    message: 'ssuccesfy login',
+    data: {
+      userFind
+    }
+  })
+})
