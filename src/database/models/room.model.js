75e000174b8e3193a253267570247255f830c3ba
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-08-31 15:53:39 -0600

    creando el model room

---
+++ src/database/models/room.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const roomSchema = new Schema({
+  token: {
+    type: String,
+  },
+  startTime: {
+    type: Date,
+    defaul: new Date(),
+  },
+  endTime: {
+    type: Date,
+  },
+  points: {
+    type: Number,
+  },
+  places: {
+    type: Number,
+    usersFirst: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  }
+})
+
+const Room = mongoose.model('Room', roomSchema)
+
+module.exports = { Room }
