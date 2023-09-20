cef7c491b8622a112b6e43b7d900413ce9b2255a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-19 20:38:02 -0600

    quitando los conosle.log

--- src/middlewares/validationsBody.middleware.js
+++ src/middlewares/validationsBody.middleware.js
@@ -5,17 +5,16 @@ const { ErrorApp } = require('../utils/ErroAppr')
 const validate = validations => {
   return async (req, res, next) => {
     for (validation of validations) {
-      console.log(validation.fields)
+
       const result = await validation.run(req)
       if (result.errors.length) break;
     }
 
     const errors = validationResult(req)
+    
     if (errors.isEmpty()) return next()
 
-    // res.status(400).json({ errors: errors.array()})
-
-    next(new ErrorApp(errors, 404))
+    return next(new ErrorApp(errors.array(), 400))
   }
 }
 
