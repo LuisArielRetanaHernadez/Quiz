453303a87cd0a16fd72740d928e94dc900c71a11
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2023-09-21 16:09:11 -0600

    creando el router '/:id/create/quiz' con los middleware authUser y protectRoom

--- src/routers/room.router.js
+++ src/routers/room.router.js
@@ -2,11 +2,12 @@ const express = require('express')
 
 // controllers
 const {
-  createRoom
+  createRoom, 
+  createQuiz
 } = require('../controllers/room.controller')
 
 // middleware
-const { authUser } = require('../middlewares/auth.middleware')
+const { authUser, protectRoom } = require('../middlewares/auth.middleware')
 const { validate } = require('../middlewares/validationsBody.middleware')
 
 // body
@@ -15,5 +16,6 @@ const { create } = require('../middlewares/express_validator/roomBody.middleware
 const router = express.Router()
 
 router.post('/', authUser, validate(create), createRoom)
+router.post('/:id/create/quiz', authUser, protectRoom, createQuiz)
 
 module.exports = { routerRoom: router}
